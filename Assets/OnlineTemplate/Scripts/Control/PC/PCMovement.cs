42bf518ce4060dc1fe11c8cc34fd1e5fec2cfefa
Saranpong <saranpongk@gmail.com>
2022-02-21 15:22:14 +0700

    Add 3rd person with animation

--- Assets/OnlineTemplate/Scripts/Control/PC/PCMovement.cs
+++ Assets/OnlineTemplate/Scripts/Control/PC/PCMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PCMovement : MonoBehaviour
 {
     private PCControl pcControl;
+    [SerializeField] private Animator animator;
+
     Vector2 moveInput;
+
     public float moveSpeed = .5f;
     public Transform HeadCamera;
+
+    private int runMultiplier = 1;
     private void Awake()
     {
         pcControl = new PCControl();
@@ -22,12 +27,30 @@ public class PCMovement : MonoBehaviour
         pcControl.Player.Disable();
     }
 
+    private void Update()
+    {
+        pcControl.Player.Run.performed += _ => Run(true);
+        pcControl.Player.Run.canceled += _ => Run(false);
+    }
+
     private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    public void HandleMovement()
     {
         moveInput = pcControl.Player.Move.ReadValue<Vector2>();
         transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.deltaTime);
         transform.rotation = Quaternion.Euler(0, HeadCamera.rotation.eulerAngles.y, 0);
+
+        animator.SetFloat("XAxis", moveInput.x);
+        animator.SetFloat("YAxis", moveInput.y);
     }
 
-
+    private void Run(bool isRun)
+    {
+        animator.SetBool("Run", isRun);
+        moveSpeed = isRun ? 1.5f : .5f;
+    }
 }
